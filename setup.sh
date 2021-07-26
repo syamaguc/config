90a8c242a8da400838cb4c5823b1e849284b9f9e
syamaguc <programmer2844@gmail.com>
2021-07-26 18:46:40 +0900

    🐛  Fix app name difference in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-APPS=(stow ag rg)
+COMMON=(stow)
+MAC=(ag rg)
+UBUNTU=(silversearcher-ag ripgrep)
 
 # Mac OS
 if [ "$(uname)" == 'Darwin' ]; then
     brew update && brew upgrade && brew cleanup && brew autoremove
-	for app in ${APPS[@]}; do
+	for app in ${COMMON[@]}; do
 	  brew install $app
 	  echo "install $app"
 	done
@@ -13,9 +15,13 @@ if [ "$(uname)" == 'Darwin' ]; then
 # Linux 
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     # Ubuntu 
-    sudo apt update && sudo apt upgrade
+    # sudo apt update -y && sudo apt upgrade -y
 	if [ "$(expr substr $(cat /etc/*-release | grep "^NAME=") 7 6)" == 'Ubuntu' ]; then
-		for app in ${APPS[@]}; do
+		for app in ${COMMON[@]}; do
+		  sudo apt install $app
+		  echo "install $app"
+		done
+		for app in ${UBUNTU[@]}; do
 		  sudo apt install $app
 		  echo "install $app"
 		done
