1f1f92f5ff8a433ae7d5a9467bae82a150d03409
syamaguc <programmer2844@gmail.com>
2022-09-29 13:35:13 +0900

    update setup-arch.sh

--- script/setup-arch.sh
+++ script/setup-arch.sh
@@ -1,89 +1,134 @@
 #!/bin/bash
 
-BASE=(\
-      # basic item
-      stow tmux git man htop zsh \
-      # terminal
-      alacritty \
-      # for japanese
-      fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-qt fcitx5-gtk fcitx5-nord \
-      # i3 & important apps
-      i3 i3blocks feh rofi ranger conky picom \
-      # lightdm
-      lightdm lightdm-gtk-greeter \
-      # network
-      net-tools networkmanager network-manager-applet \
-      # Misc
-      unzip wget nautilus xorg xsel jq alsa-utils neofetch xdg-user-dirs \
-      # for language
-      npm yarn rust go \
-      # for python
-      python-pip \
-      # for vim
-      neovim ripgrep fzf shellcheck \
-      # fonts
-      otf-ipafont noto-fonts noto-fonts-emoji \
-      # sound
-      pulseaudio pulseaudio-alsa pavucontrol \
-      # apps
-      discord \
-      )
+BASE=(
+	# basic item
+	stow tmux git man htop zsh
+	# terminal
+	alacritty
+	# for japanese
+	fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-qt fcitx5-gtk fcitx5-nord
+	# i3 & important apps
+	i3 i3blocks feh rofi ranger conky picom
+	# lightdm
+	lightdm lightdm-gtk-greeter
+	# network
+	net-tools networkmanager network-manager-applet
+	# Misc
+	unzip wget nautilus xorg xsel jq alsa-utils neofetch xdg-user-dirs
+	# for language
+	npm yarn rust go
+	# for python
+	python-pip
+	# for vim
+	neovim ripgrep fzf shellcheck shfmt stylua
+	# fonts
+	otf-ipafont noto-fonts noto-fonts-emoji
+	# sound
+	pulseaudio pulseaudio-alsa pavucontrol
+	# apps
+	discord
+	# dev
+	docker docker-compose
+)
 
-AUR=(\
-      google-chrome \
-      dropbox \
-      ttf-ricty-diminished \
-      autotiling \
-      slack-desktop \
-      snapd \
-      )
+AUR=(
+	google-chrome
+	dropbox
+	ttf-ricty-diminished
+	autotiling
+	slack-desktop
+	snapd
+)
 
-# TODO:
-function lightdm-setting () {
-      # Fix failed to start lightdm @AMD processor
-      sudo sed -i 's/^\(#?logind\)-check-graphical\s*=\s*\(.*\)/logind-check-graphical = true #\1/g' /etc/lightdm/lightdm.conf
-      systemctl enable lightdm
-      systemctl start lightdm
+function pacman-install() {
+	echo "Install basics ...\n"
+
+	for app in ${BASE[@]}; do
+		sudo pacman -Sy --noconfirm --needed $app
+	done
+
+	which yay >/dev/null 2>&1
+	case $? in
+	0) ;;
+
+	1)
+		echo "Install yay ...\n"
+		mkdir -p $HOME/tmp
+		cd $HOME/tmp
+		git clone https://aur.archlinux.org/yay-bin.git
+		cd yay-bin
+		makepkg --noconfirm -si
+		cd -
+		sudo rm -rf $HOME/tmp/yay-bin
+		;;
+	esac
+
 }
 
-function tmux-setting () {
-      if [ -f $HOME/.tmux/plugins/tpm ]; then
-            echo "plugin already exist";
-      else
-            git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-      fi
+function yay-install() {
+	echo "Install AUR packages ...\n"
+	for app in ${AUR[@]}; do
+		yay -Sy --noconfirm --needed $app
+	done
+
 }
 
-echo "Install basics ...\n"
+function lightdm-setting() {
+	# Fix failed to start lightdm @AMD processor
+	sudo sed -i 's/^\(#?logind\)-check-graphical\s*=\s*\(.*\)/logind-check-graphical = true #\1/g' /etc/lightdm/lightdm.conf
+	sudo systemctl enable lightdm
+	sudo systemctl start lightdm
+}
 
-for app in ${BASE[@]}; do
-  sudo pacman -Sy --noconfirm --needed $app
-done
+function docker-setting() {
+	sudo groupadd docker
+	sudo usermod -aG docker "$USER"
+	newgrp docker
+	sudo systemctl enable docker.service
+	sudo systemctl enable containerd.service
+}
 
-which yay >/dev/null 2>&1
-case $? in
-      0)
-            ;;
-      1)
-            echo "Install yay ...\n"
-            mkdir -p $HOME/tmp
-            cd $HOME/tmp
-            git clone https://aur.archlinux.org/yay-bin.git
-            cd yay-bin
-            makepkg --noconfirm -si
-            cd -
-            sudo rm -rf $HOME/tmp/yay-bin
-            ;;
-esac
+function tmux-setting() {
+	if [ -f $HOME/.tmux/plugins/tpm ]; then
+		echo "plugin already exist"
+	else
+		git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+	fi
+}
 
-echo "Install AUR packages ...\n"
-for app in ${AUR[@]}; do
-  yay -Sy --noconfirm --needed $app
-done
+function node-setting() {
+	mkdir ~/.npm-global
+	npm config set prefix "$HOME/.npm-global"
+	zsh
+	npm install -g neovim prettier serverless
+}
+
+function python-setting() {
+	pip install -U pip
+	pip install neovim black isort flake8
+}
+
+function aws-setting() {
+	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	unzip awscliv2.zip
+	sudo ./aws/install
+	rm -rf awscliv2.zip
+	rm -rf aws
+}
+
+function other-setting() {
+	sudo chsh -s /bin/zsh
+	sudo systemctl enable NetworkManager
+	sudo systemctl start NetworkManager
+}
+
+pacman-install
+yay-install
 
 lightdm-setting
 tmux-setting
-chsh -s /bin/zsh
-systemctl start NetworkManager
-
-echo "Finish, all done.\n"
+docker-setting
+node-setting
+python-setting
+other-setting
+aws-setting
