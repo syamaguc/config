5bd23249efa72f2f2ec172e696b855fe1d6f1869
syamaguc <programmer2844@gmail.com>
2023-10-19 17:10:25 +0900

    chore

--- nvim/.config/nvim/lua/plugins/syamaguc.lua
+++ nvim/.config/nvim/lua/plugins/syamaguc.lua
@@ -5,14 +5,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- add symbols-outline
-  {
-    "simrat39/symbols-outline.nvim",
-    cmd = "SymbolsOutline",
-    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-    config = true,
-  },
-
   -- override nvim-cmp and add cmp-emoji
   {
     "hrsh7th/nvim-cmp",
