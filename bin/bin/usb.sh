361f3cf41fb1ecc04f195e3fd10a262b1c14ec7e
syamaguc <programmer2844@gmail.com>
2023-11-09 00:50:24 +0900

    chore

--- bin/bin/usb.sh
+++ bin/bin/usb.sh
@@ -2,7 +2,10 @@
 
 if [ "$1" == "mount" ]; then
 	sudo mkdir -p /mnt/usb
-	sudo mount /dev/disk/by-uuid/5FD7-2391 /mnt/usb -o umask=000
+	# buffalo
+	#sudo mount /dev/disk/by-uuid/5FD7-2391 /mnt/usb -o umask=000
+	# microsd
+	sudo mount /dev/disk/by-uuid/6AC9-83FD /mnt/usb -o umask=000
 fi
 
 if [ "$1" == "remove" ]; then
