35f5d958ca528f0541d2a8314b2bc855fb180c31
syamaguc <programmer2844@gmail.com>
2024-03-22 13:50:38 +0900

    chore

---
+++ nvim/.config/nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,13 @@
+return {
+  "tokyonight.nvim",
+  lazy = true,
+  opts = function()
+    return {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
+  end,
+}
