14b70df13d5c56eba998c410883f87a9cac46b5e
syamaguc <programmer2844@gmail.com>
2024-04-04 16:10:36 +0900

    chore

--- bin/bin/ssh-keygen.sh
+++ bin/bin/ssh-keygen.sh
@@ -1,1 +1,7 @@
-ssh-keygen -t ed25519 -C "$(hostname)"
+#! /bin/bash
+
+if [ $# -eq 1 ]; then
+	ssh-keygen -t ed25519 -C "$@" -f $HOME/.ssh/"$@"_ed25519
+else
+	ssh-keygen -t ed25519 -C "$(hostname)" -f $HOME/.ssh/$(hostname)_ed25519
+fi
