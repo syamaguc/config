faca22f0d6bffaef4d8c6ba2fa2386c0583a8303
syamaguc <programmer2844@gmail.com>
2024-07-16 09:40:35 +0900

    chore

--- zsh/.config/zsh/plugin.zsh
+++ zsh/.config/zsh/plugin.zsh
@@ -1,4 +1,4 @@
 # Plugins
+zsh_add_plugin "hlissner/zsh-autopair"
 zsh_add_plugin "zsh-users/zsh-autosuggestions"
 zsh_add_plugin "zsh-users/zsh-syntax-highlighting"
-zsh_add_plugin "hlissner/zsh-autopair"
